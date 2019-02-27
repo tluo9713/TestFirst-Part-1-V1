939bcb8c2c1bed57b8af9de8cc8b70beb8263761
Thomas Luo <tluo9713@gmail.com>
2019-02-26 22:48:14 -0500

    00 Reading Test Specs Completed

--- 00-Reading-Test-Specs/readingSpec.js
+++ 00-Reading-Test-Specs/readingSpec.js
@@ -1,10 +1,10 @@
 describe('the function `hello`', function() {
   it('says hello', function() {
-    expect(hello()).toEqual(FILL_ME_IN);
+    expect(hello()).toEqual("Hello!");
   });
 
   it('says hello to someone', function() {
-    expect(hello('Fred')).toEqual(FILL_ME_IN);
+    expect(hello('Fred')).toEqual("Hello, Fred!");
   });
 });
 
@@ -13,6 +13,7 @@ describe('the function `add`', function(){
   var returnValue = 0;
 
   beforeEach(function(){
+    returnValue = 0;
     // re-assign returnValue to 0 before each spec
   });
 
